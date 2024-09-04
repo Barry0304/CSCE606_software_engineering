b445a372fe9b6e27e1f30eb280c945f9335c53f6
Barry0304 <barry89130663@gmail.com>
2024-09-03 23:54:32 -0500

    finished!

--- hw1/lib/ruby_intro.rb
+++ hw1/lib/ruby_intro.rb
@@ -1,36 +1,47 @@
-# frozen_string_literal: true
-
-# Part 1
-
 def sum(arr)
-  # YOUR CODE HERE
+	arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+	return 0 if arr.empty?
+	return arr[0] if arr.length == 1
+	return arr.max(2).sum
 end
 
-def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+def sum_to_n?(arr, num)
+	return false if arr.empty?
+	have = false
+	arr.each do |e|
+		have = have || (arr.include?(num - e) && ((num - e)!=e)) 
+	end
+	return have
 end
 
-# Part 2
-
-def hello(name)
-  # YOUR CODE HERE
+def hello(str)
+	return "Hello, #{str}"
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+	return false if string.empty?  # 檢查字串是否為空
+	return !!(string[0] =~ /^[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/)
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+	return false unless string.match?(/\A[01]+\z/)
+	string.to_i(2) % 4 == 0
 end
 
-# Part 3
-
-# Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+	attr_accessor :isbn, :price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 'ISBN cannot be empty' if isbn.empty?
+		raise ArgumentError, 'Price must be greater than zero' if price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		format('$%.2f', @price)
+	end
 end
